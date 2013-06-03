ee817ca16ca82a62eddd8d656ca3daf5235c658c
Federico Ruiz Ugalde <fruiz@eie.ucr.ac.cr>
2013-06-02 18:58:08 -0600

    The example runs on interrupts and is able to respond anything back.

---
+++ src/cdcacm_example.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2013 ARCOS-Lab Universidad de Costa Rica
+ * Author: Federico Ruiz Ugalde <memeruiz@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/f4/rcc.h>
+#include <libopencm3/stm32/f4/gpio.h>
+#include "cdcacm_example.h"
+#include "cdcacm.h"
+
+void system_init(void) {
+	rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_120MHZ]);
+
+	rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPAEN);
+	rcc_peripheral_enable_clock(&RCC_AHB2ENR, RCC_AHB2ENR_OTGFSEN);
+
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE,
+			GPIO9 | GPIO11 | GPIO12);
+	gpio_set_af(GPIOA, GPIO_AF10, GPIO9 | GPIO11 | GPIO12);
+}
+
+int main(void)
+{
+  system_init();
+  cdcacm_init();
+  while (1){
+    //cdcacm_poll();
+  }
+}
+
